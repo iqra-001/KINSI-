e32102fb857944c5808e3a1218a571210412990e
iqra <abdikarimiqra@gmail.com>
2025-08-07 10:42:00 +0300

    assign

--- KINSI/src/App.jsx
+++ KINSI/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './App.css';
 import Landing from './Components/Landing';
+import KinsiLandingPage from './Components/Landing';
 
 function App() {
   return (
   
-      <Landing/>
+      <KinsiLandingPage/>
     
   )
 }
